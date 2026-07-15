2efdcd8364a2d532a614361d2ef46ca8a5cd9d20
Karan Srivastava <er.karansrivastava@gmail.com>
2026-07-15 15:25:40 +0530

    Added GTM snippet

--- community-website/app/layout.tsx
+++ community-website/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -22,6 +23,21 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} antialiased`}
     >
+      <head>
+        <Script
+          id="gtm"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MXGNJXF2');
+            `,
+          }}
+        />
+      </head>
       <body className="font-sans overflow-x-hidden">{children}</body>
     </html>
   );
